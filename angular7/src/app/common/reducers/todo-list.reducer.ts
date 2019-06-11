1af2c99154f72e022b2743c0262c56483d9d84a8
Andrew Blomquist <andrew.blomquist@fmr.com>
2019-06-11 12:41:10 -0600

    reorganizing reduces so it looks cleaner

--- angular7/src/app/common/reducers/todo-list.reducer.ts
+++ angular7/src/app/common/reducers/todo-list.reducer.ts
@@ -15,51 +15,79 @@ export const initialState: TodoListState = {
 
 export function todoListReducer(state = initialState, action: TodoListActions): TodoListState {
   switch (action.type) {
-    // TODO: is it appropriate to have a reducer interact with data persistence?
     case TodoListActionTypes.LoadTodoList:
-      let temp = JSON.parse(localStorage.getItem(state.storageKey));
-      if (temp === null) {
-        temp = [];
-      }
-      return {
-        ...state,
-        list: temp
-      };
+      return applyLoadTodoList(state, action);
     case TodoListActionTypes.AddTodo:
-      return {
-        ...state,
-        list: [...state.list, action.todo]
-      };
+      return applyAddTodo(state, action);
     case TodoListActionTypes.UpdateTodo:
-      const newList = state.list.slice();
-      newList[action.index] = action.todo;
-      return {
-        ...state,
-        list: newList
-      };
+      return applyUpdateTodo(state, action);
     case TodoListActionTypes.RemoveTodo:
-      state.list.splice(action.index, 1);
-      return {
-        ...state,
-        list: state.list
-      };
+      return applyRemoveTodo(state, action);
     case TodoListActionTypes.RemoveCompleted:
-      return {
-        ...state,
-        list: state.list.filter((todo) => {
-          return !todo.completed;
-        })
-      };
+      return applyRemoveCompleted(state, action);
     case TodoListActionTypes.UpdateEditingTodo:
-      return {
-        ...state,
-        editingTodo: action.todo
-      };
-    // TODO: is it appropriate to have a reducer interact with data persistence?
+      return applyUpdateEditingTodo(state, action);
     case TodoListActionTypes.SaveToLocalStorage:
-      localStorage.setItem(state.storageKey, JSON.stringify(state.list));
-      return state;
+      return applySaveToLocalStorage(state, action);
     default:
       return state;
   }
+}
+
+// TODO: is it appropriate to have a reducer interact with data persistence?
+function applyLoadTodoList(state, action) {
+  let temp = JSON.parse(localStorage.getItem(state.storageKey));
+  if (temp === null) {
+    temp = [];
+  }
+  return {
+    ...state,
+    list: temp
+  };
+}
+
+function applyAddTodo(state, action) {
+  return {
+    ...state,
+    list: [...state.list, action.todo]
+  };
+}
+
+function applyUpdateTodo(state, action) {
+  const newList = state.list.slice();
+  newList[action.index] = action.todo;
+  return {
+    ...state,
+    list: newList
+  };
+}
+
+function applyRemoveTodo(state, action) {
+  state.list.splice(action.index, 1);
+  return {
+    ...state,
+    list: state.list
+  };
+}
+
+function applyRemoveCompleted(state, action) {
+  return {
+    ...state,
+    list: state.list.filter((todo) => {
+      return !todo.completed;
+    })
+  };
+}
+
+function applyUpdateEditingTodo(state, action) {
+  return {
+    ...state,
+    editingTodo: action.todo
+  };
+}
+
+// TODO: is it appropriate to have a reducer interact with data persistence?
+function applySaveToLocalStorage(state, action) {
+  localStorage.setItem(state.storageKey, JSON.stringify(state.list));
+  return state;
 }
