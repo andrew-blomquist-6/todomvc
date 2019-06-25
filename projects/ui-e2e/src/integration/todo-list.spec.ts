97be176d24f0a140435683ad47017cf7b3944415
Andrew Blomquist <andrew.blomquist@fmr.com>
2019-06-25 15:51:29 -0600

    adding more e2e tests

--- projects/ui-e2e/src/integration/todo-list.spec.ts
+++ projects/ui-e2e/src/integration/todo-list.spec.ts
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-import {addTodo, verifyTodoList} from '../support/app.po';
+import {addTodo, removeTodo, toggleTodo, verifyTodoList} from '../support/app.po';
 
 describe('The TODO List', () => {
 
@@ -9,20 +9,52 @@ describe('The TODO List', () => {
   });
 
   it('should let you add a todo', () => {
-    const todo = 'here is the first todo';
-    addTodo(todo);
+    addTodo('here is the first todo');
     verifyTodoList();
   });
 
   it('should add a new todo to the end of the list', () => {
-    const todo = 'the second todo in the list';
-    addTodo(todo);
+    addTodo('the second todo in the list');
     verifyTodoList();
   });
 
   it( 'should preserve the order todos were added to the list', () => {
-    const todo = 'the third todo in the list';
-    addTodo(todo);
+    addTodo('the third todo in the list');
+    addTodo('number 4');
+    addTodo('and then there were five');
     verifyTodoList();
   });
+
+  it('should remove the todo when you click on its remove button', () => {
+    removeTodo('the second todo in the list');
+    verifyTodoList();
+  });
+
+  it('should mark the todo completed when the user clicks the checkbox', () => {
+    toggleTodo('the third todo in the list');
+    verifyTodoList();
+  });
+
+  it('should show how many todos are incomplete', () => {
+    cy.get('.footer > .todo-count').should('contain', '3 items left');
+    toggleTodo('the third todo in the list');
+    verifyTodoList();
+    cy.get('.footer > .todo-count').should('contain', '4 items left');
+    toggleTodo('the third todo in the list');
+    toggleTodo('and then there were five');
+    verifyTodoList();
+    cy.get('.footer > .todo-count').should('contain', '2 items left');
+    removeTodo('the third todo in the list');
+    removeTodo('number 4');
+    verifyTodoList();
+    cy.get('.footer > .todo-count').should('contain', '1 item left');
+  });
+
+  it('should show only incomplete todos when the user clicks \'Active\'', () => {});
+
+  it('should show only complete todos when the user clicks\'Completed\'', () => {});
+
+  it('should toggle all todos when the toggle all switch is clicked', () => {});
+
+  it('should remove all completed todos when \'Clear completed\' is clicked', () => {});
 });
--- projects/ui-e2e/src/support/app.po.ts
+++ projects/ui-e2e/src/support/app.po.ts
@@ -1,14 +1,32 @@
 /// <reference types="cypress" />
 
-const todoList = [];
+import {Todo} from '../../../ui/src/app/common/todo.model';
+
+const todoList: Todo[] = [];
 
 export function addTodo(todo: string) {
   cy.get('.new-todo').type(`${todo}{enter}`);
-  todoList.push(todo);
+  todoList.push(new Todo(false, todo));
+}
+
+export function removeTodo(todo: string) {
+  const index = todoList.findIndex(i => i.title === todo);
+  cy.get(`:nth-child(${index + 1}) > app-todo > .view > .destroy`)
+    .invoke('show')
+    .click();
+  todoList.splice(index, 1);
+}
+
+export function toggleTodo(todo: string) {
+  const index = todoList.findIndex(i => i.title === todo);
+  cy.get(`:nth-child(${index + 1}) > app-todo > .view > .toggle`).click();
+  const obj = todoList[index];
+  todoList[index] = new Todo(!obj.completed, todo);
 }
 
 export function verifyTodoList() {
   todoList.forEach((todo, index) => {
-    cy.get(`:nth-child(${index + 1}) > app-todo > .view > label`).should('contain', todo);
+    cy.get(`:nth-child(${index + 1}) > app-todo > .view > label`).should('contain', todo.title);
+    cy.get(`:nth-child(${index + 1}) > app-todo > .view > .toggle`).should('have.value', todo.completed.toString());
   });
 }
